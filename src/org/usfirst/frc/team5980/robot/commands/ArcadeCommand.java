cffa20318ad7ac40a6e1ae7940c287c8772b4368
unknown <matt@salespad.net>
2018-01-13 13:45:07 -0500

    first commit

---
+++ src/org/usfirst/frc/team5980/robot/commands/ArcadeCommand.java
@@ -0,0 +1,78 @@
+package org.usfirst.frc.team5980.robot.commands;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import org.usfirst.frc.team5980.robot.Robot;
+
+/**
+ * This command allows the robot to be controlled via the joy stick.
+ */
+public class ArcadeCommand extends Command {
+	public ArcadeCommand() {
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.driveSubsystem);
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		System.out.println("ArcadeCommand.initialize");
+	}
+
+    public double deadBand(double x){
+    	if (Math.abs(x)<.2){
+    		return 0;
+    	}else{
+    		return x;
+    	}
+    }
+    
+    public double clip(double x){
+    	if (x>.885) return .885;
+    	if (x<-.885) return -.885;
+    	return x;
+    }
+    
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		//System.out.println("ArcadeCommand.execute");
+		
+		double throttle = deadBand(-Robot.oi.driver.getRawAxis(1)) * Robot.driveSubsystem.speedType;
+		double wheel = deadBand(Robot.oi.driver.getRawAxis(4)) * Robot.driveSubsystem.speedType;
+		double leftPower = clip (throttle + wheel);
+		double rightPower = clip (throttle - wheel);
+		Robot.driveSubsystem.setPower(leftPower, rightPower);
+		
+		//SmartDashboard.putNumber("Left Encoder: ", Robot.sensors.getLeftEncoder());
+		//SmartDashboard.putNumber("Right Encoder: ", Robot.sensors.getRightEncoder());
+		
+		//check the various ways to get coordinates from the xboxController
+		//System.out.println( Robot.oi.driver.getRawAxis(AxisType.kX.value) );
+		//System.out.println( Robot.oi.driver.getY(Hand.kLeft) );
+		//System.out.println( Robot.oi.driver.getX(Hand.kRight) );				
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		//System.out.println("ArcadeCommand.isFinished");
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		System.out.println("ArcadeCommand.end");
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+		System.out.println("ArcadeCommand.interrupted");
+	}
+}
---
+++ src/org/usfirst/frc/team5980/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,56 @@
+package org.usfirst.frc.team5980.robot.subsystems;
+
+import org.usfirst.frc.team5980.robot.commands.ArcadeCommand;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+/**
+ * Main subsystem for driving/moving the robot via the talons and motors
+ */
+public class DriveSubsystem extends Subsystem {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	TalonSRX left1, left2, right1, right2;
+ 
+	public double speedType;
+	public boolean type;
+	public DriveSubsystem() {
+		right1 = new TalonSRX(1);
+		right2 = new TalonSRX(2);
+		left1 = new TalonSRX(3);
+		left2 = new TalonSRX(4);
+		//right1.setInverted(true);
+		//right2.setInverted(true);
+		speedType = 1;
+		type = true;
+	}
+	public void toggleSpeedType() {
+		if (speedType == 1) {
+			speedType = .5;
+		}
+		else if (speedType == .5) {
+			speedType = 1;
+		}
+		//SmartDashboard.putNumber("speedType ", speedType);
+	}
+	public void setPower(double left, double right) {
+		System.out.println("DriveSubsystem.setPower");
+		System.out.println(left);
+		System.out.println(right);
+
+		
+		left1.set(com.ctre.phoenix.motorcontrol.ControlMode.PercentOutput, left);
+		left2.set(com.ctre.phoenix.motorcontrol.ControlMode.PercentOutput, left);
+		right1.set(com.ctre.phoenix.motorcontrol.ControlMode.PercentOutput, right);
+		right2.set(com.ctre.phoenix.motorcontrol.ControlMode.PercentOutput, right);
+	}
+	
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		this.setDefaultCommand(new ArcadeCommand());
+	}
+}
