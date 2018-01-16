5073d56fad1e54cabd6ad3d812440125c95c1208
Nathan-Strodtbeck <strodtbecks@outlook.com>
2018-01-16 18:55:29 -0500

    Changed directories Moved PID to utils folder

--- src/org/usfirst/frc/team5980/robot/commands/DriveToPointCommand.java
+++ src/org/usfirst/frc/team5980/robot/commands/DriveToPointCommand.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team5980.robot.commands;
 
-import org.usfirst.frc.team5980.robot.EGRPID;
 import org.usfirst.frc.team5980.robot.Robot;
 import org.usfirst.frc.team5980.robot.utils.Acceleration;
+import org.usfirst.frc.team5980.robot.utils.EGRPID;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
