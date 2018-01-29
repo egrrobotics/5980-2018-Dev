201abeb12b44a3b2bf325afc269f5a16bd0562cd
carliecouz <c.couzens5@gmail.com>
2018-01-29 17:21:11 -0500

    Psuedo code for Left left position 2 and Right right position 3

--- src/org/usfirst/frc/team5980/robot/commands/FmsRrAutoCommandPosition3.java
+++ src/org/usfirst/frc/team5980/robot/commands/FmsRrAutoCommandPosition3.java
@@ -1,36 +1,42 @@
 package org.usfirst.frc.team5980.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
  *
  */
-public class FmsRrAutoCommandPosition3 extends Command {
+public class FmsRrAutoCommandPosition3 extends CommandGroup {
 
     public FmsRrAutoCommandPosition3() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+       
+    	/* Place cube on right side of switch, grab cube behind switch, place cube on scale
+    	 * (Denoted by all commands)
+    	 * or
+    	 * Place cube on right scale
+    	 * (Denoted by all commands without "//")
+    	
+    	addSequential(new DriveForwardAutoCommand(power, distance, heading));
+    	//addSequential(new RotateToHeadingCommand(heading, power));
+    	//addSequential(new DriveForwardAutoCommand(p, d, h));
+    	//addParallel(new ElevatorLiftCommand(lift for time));
+    	//addSequential(new DropCubeCommand(power));
+    	//addSequential(new DriveBackwardsAutoCommand(p, d, h));
+    	//addParallel(new ElevatorDropCommand(drop for time));
+    	
+    	//addSequential(new DriveForwardAutoCommand(p, d, h));
+    	//addSequential(new RotateToHeadingCommand(h, p));
+    	//addSequential(new DriveForwardAutoCommand(p, d, h));
+    	//addParallel(new GrabCubeCommand(power));
+    	//addSequential(new DriveBackwardsCommand(p, d, h));
+    	//addSequential(new RotateToHeadingCommand(h, p));
+    	
+    	//addSequential(new DriveForwardAutoCommand(p, d, h));
+    	addSequential(new RotateToHeadingCommand(h, p));
+    	addSequential(new DriveForwardAutoCommand(p, d, h));
+    	addParalllel(new ElevatorLiftCommand(lift for time)); 
+    	addSequential(new DropCubeCommand(power));
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+    	 */
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
     }
 }
