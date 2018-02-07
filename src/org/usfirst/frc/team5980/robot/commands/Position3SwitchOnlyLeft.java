6aa9e30b427687f474c0c0fed55b87662d55a6d0
Ludwant <antonludwig01@yahoo.com>
2018-02-06 20:13:54 -0500

    Added new files for switch only autos on both sides, and fiddled some more with stop PID tuning.

---
+++ src/org/usfirst/frc/team5980/robot/commands/Position3SwitchOnlyLeft.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team5980.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class Position3SwitchOnlyLeft extends CommandGroup {
+
+    public Position3SwitchOnlyLeft() {
+    	addSequential(new DriveForwardAuto(.3, 160, 0, .000125));
+    	addSequential(new PauseAuto(300));
+    	addSequential(new RotateToHeading(.1, 90));
+    	addSequential(new PauseAuto(300));
+    	addSequential(new DriveForwardAuto(.3, 150, 0, .000125));
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
